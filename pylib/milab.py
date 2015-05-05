01420b6d18c116a57a8a9f2d42cd1f7d2d5a683b
yaqiang <yaqiang.wang@gmail.com>
2015-05-05 13:31:53 +0800

    move out milab

--- pylib/milab.py
+++
@@ -1,9 +0,0 @@
-import mipylib
-#from org.meteoinfo.numeric.JNumeric import *
-from mipylib.minum import *
-from mipylib.miplot import *
-from mipylib.midata import *
-#import org.meteoinfo.numeric.JNumeric as np
-import mipylib.minum as np
-import mipylib.miplot as plt
-import mipylib.midata as ds
--- pylib/milab_s.py
+++
@@ -1,3 +0,0 @@
-import org.meteoinfo.numeric.JNumeric as np
-import mipylib.miplot as plt
-import mipylib.midata as ds
